dc763940a736c8095c20964bfa521bfc1aafad9b
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-16 01:45:52 +0300

    Add rectangle area calculator script

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+# Rectangle Area Calculator
+
+# تعريف المتغيرات
+length = 10
+width = 5
+
+# حساب مساحة المستطيل
+area = length * width
+
+# طباعة النتيجة
+print("The area of the rectangle is:", area)
+# طباعة النتيجة
